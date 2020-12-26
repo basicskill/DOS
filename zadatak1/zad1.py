7502ee25bf3d6b11db17e852b8e49e5ce56827e8
mladen <mladenbasic99@gmail.com>
2020-12-26 11:35:10 +0100

    np.arange bug fix + graph saving

--- zadatak1/zad1.py
+++ zadatak1/zad1.py
@@ -21,6 +21,7 @@ def lin_conv(x, y):
     return lin
 
 def circ_conv(x, y):
+    N = len(x)
     circ = np.zeros(N)
     for n in range(N):
         circ[n] = sum([x[k] * y[(n - k) % N] for k in range(N)])
@@ -33,23 +34,22 @@ if __name__ == "__main__":
     x[N//2:] = 1 - x[N//2:] - N/2
 
     # Definicija y[n] signala
-    y = np.arange(N)
-    y[:N//2] = 2 * np.cos((P + 1)*y[:N//2] + np.pi/4)
+    y = np.arange(N, dtype=float)
+    y[:N//2] = 2.0 * np.cos((P + 1)*y[:N//2] + np.pi/4)
     y[N//2:] = 0
 
     # Plot ulaznih signala
     plt.stem(x, linefmt='gray', markerfmt='D')
     plt.stem(y, linefmt='gray', markerfmt='D')
+    plt.savefig('figures/zad1_signali.png')
 
     # Linearna konvolucija
     lin = lin_conv(x, y)
 
     # Plot linearne konvolucije, implementirana naspram integrisana
     plt.figure()
-    plt.subplot(2, 1, 1)
     plt.stem(lin, linefmt='gray', markerfmt='D')
-    plt.subplot(2, 1, 2)
-    plt.stem(np.convolve(x, y), linefmt='gray', markerfmt='D')
+    plt.savefig('figures/zad1_linearna_konvolucija.png')
 
     # Cirkularnu konvoluciju
     circ = circ_conv(x, y)
@@ -57,10 +57,10 @@ if __name__ == "__main__":
     # Cirkularna konvolucija
     plt.figure()
     plt.stem(circ, linefmt='gray', markerfmt='D')
+    plt.savefig('figures/zad1_ciklicna_konvolucija.png')
 
 
-    overlap = np.isin(lin, circ)
-    plt.figure()
-    plt.stem(circ, linefmt='gray', markerfmt='D')
-    plt.stem(lin * overlap, linefmt='gray', markerfmt='D')
-    plt.show()
+    # overlap = np.isin(lin, circ)
+    # plt.figure()
+    # plt.stem(circ, linefmt='gray', markerfmt='D')
+    # plt.stem(lin * overlap, linefmt='gray', markerfmt='D')
